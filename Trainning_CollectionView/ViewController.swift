7d16b39dc818c2857fd2e55e322706e00c17cdfc
Keisuke2162 <applabo.kei@gmail.com>
2020-06-19 20:46:25 +0900

    Initial Commit

---
+++ Trainning_CollectionView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Trainning_CollectionView
+//
+//  Created by 植田圭祐 on 2020/06/19.
+//  Copyright © 2020 Keisuke Ueda. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
